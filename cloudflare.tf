7d86a197789391864d8448758a1432e1b9106d8d
Andrew Venenga <drewvpopc@gmail.com>
2023-07-23 19:34:38 -0500

    Migrated to terraform cloud to sync state files easier

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "drewvpopc"
+
+    workspaces {
+      name = "Venenga-net_Cloudflare"
+    }
+  }
   required_providers {
     cloudflare = {
       source = "cloudflare/cloudflare"
